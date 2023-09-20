344cf07334086b9731c88eb97b838fdb64722c0e
Wupxy <muhammad.122140193@student.itera.ac.id>
2023-09-20 12:03:58 +0700

    styling: tailwind set-up for php

--- index.php
+++ index.php
@@ -1,39 +1,163 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
-    <meta charset="utf-8">
+    <meta charset="utf-8" />
     <title>Twibbon</title>
-    <script src = "https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js">
-    </script>
-    <script src = "https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js">
-    </script>
-    <link rel="stylesheet" href="style.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/0.4.1/html2canvas.js"></script>
+    <link rel="stylesheet" href="output.css" />
+    <link
+      href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.css"
+      rel="stylesheet"
+    />
   </head>
-  <body>
-    Twibbon
-    <select id = "twibbonimg">
-      <option value="img/MabaTwibbon.png">Maba Twibbon</option>
-      <option value="img/PanitiaTwibbon.png">Panitia Twibbon</option>
-    </select>
-    <br>
-		Photo <input type="file" id="photoimg" value=""> <br>
-		Resize <input type="range" id="wh" min="0" max="100" value="50">
-		Width <input type="range" id="width" min="-300" max="300" value="50">
-		Height <input type="range" id="height" min="-300" max="300" value="50">
-		Top <input type="range" id="top" min="-500" max="500" value="0">
-		Left <input type="range" id="left" min="-500" max="500" value="0">
 
-    <hr>
-
-    <div class="card">
-      <h2>Pick A Photo</h2>
-      <div class="twibbon">
-        <img src="" id = "twibbon" alt="">
-        <img src="" id = "photo" alt="">
+  <body class="bg-gradient-to-tr from-white to-purple-900 bg-no-repeat px-2">
+    <div class="max-w-[1440px] mx-auto pb-20">
+      <div class="w-full min-h-[400px] flex justify-center items-center">
+        <h1 class="font-bold text-7xl text-center">Twibbon Maker</h1>
       </div>
-      <a href="#" id = "download">Download</a>
+
+      <div class="flex items-center justify-center flex-col w-full">
+        <h2 class="font-bold">Get Started !</h2>
+        <h2 class="font-bold">Upload your photo</h2>
+
+        <label
+          for="photoimg"
+          class="flex mt-5 flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600 max-w-[700px]"
+        >
+          <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg
+              class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 20 16"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+              />
+            </svg>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400">
+              <span class="font-semibold">Click to upload</span> or drag and
+              drop
+            </p>
+            <p class="text-xs text-gray-500 dark:text-gray-400">
+              PNG, JPG, or SVG
+            </p>
+          </div>
+
+          <input id="photoimg" type="file" class="hidden" value="" />
+        </label>
+      </div>
+
+      <div class="w-full flex justify-center items-center flex-col mt-10">
+        <label
+          for="twibbonimg"
+          class="block mb-2 text-sm font-bold text-gray-900"
+          >Select Twibbon</label
+        >
+        <select
+          id="twibbonimg"
+          class="bg-gray-900 border border-gray-300 max-w-[300px] text-sm rounded-lg block w-full p-2.5 text-white"
+        >
+          <option value="img/MabaTwibbon.png">Maba Twibbon</option>
+          <option value="img/PanitiaTwibbon.png">Panitia Twibbon</option>
+        </select>
+      </div>
+
+      <div
+        class="w-full mt-20 flex flex-wrap justify-center gap-10 items-center md:flex-row flex-col"
+      >
+        <div
+          class="setting-image p-5 w-full max-w-[600px] flex-1 flex flex-col gap-5"
+        >
+          <div>
+            <label htmlFor="wh">Resize</label>
+            <input
+              type="range"
+              id="wh"
+              min="0"
+              max="100"
+              value="50"
+              class="w-full h-2 rounded-lg appearance-none cursor-pointer bg-gray-700"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="width">Width</label>
+            <input
+              type="range"
+              id="width"
+              min="-300"
+              max="300"
+              value="50"
+              class="w-full h-2 rounded-lg appearance-none cursor-pointer bg-gray-700"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="height">Height</label>
+            <input
+              type="range"
+              id="height"
+              min="-300"
+              max="300"
+              value="50"
+              class="w-full h-2 rounded-lg appearance-none cursor-pointer bg-gray-700"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="top">Top</label>
+            <input
+              type="range"
+              id="top"
+              min="-500"
+              max="500"
+              value="0"
+              class="w-full h-2 rounded-lg appearance-none cursor-pointer bg-gray-700"
+            />
+          </div>
+
+          <div>
+            <label htmlFor="left">Left</label>
+            <input
+              type="range"
+              id="left"
+              min="-500"
+              max="500"
+              value="0"
+              class="w-full h-2 rounded-lg appearance-none cursor-pointer bg-gray-700"
+            />
+          </div>
+        </div>
+
+        <div class="card flex-1 flex justify-center items-center flex-col">
+          <div
+            class="twibbon rounded-md overflow-hidden max-w-[400px] max-h-[400px]"
+          >
+            <img src="" id="twibbon" alt="" />
+            <img src="" id="photo" alt="" />
+          </div>
+        </div>
+      </div>
+
+      <a
+        href="#"
+        id="download"
+        class="p-5 text-center bg-gray-900 max-w-[300px] mx-auto rounded-md text-white mt-10 block"
+      >
+        Download
+      </a>
     </div>
 
     <script type="text/javascript" src="script.js"></script>
+    <script src="https://cdn.tailwindcss.com"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.8.1/flowbite.min.js"></script>
   </body>
 </html>
